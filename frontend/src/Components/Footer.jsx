67098e076efa7bf929570b4321fe9dda990ebbf9
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2022-11-16 02:46:09 +0530

    Create Footer.jsx

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FooterBottom from "./FooterBottom";
+import FooterTop from "./FooterTop";
+
+export default function Footer() {
+  return (
+    <div>
+      <FooterTop />
+      <FooterBottom />
+    </div>
+  );
+}
