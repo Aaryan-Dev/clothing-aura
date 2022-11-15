2a76d554ac1034a90bcca90c4b33832e4d0154b7
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2022-11-16 03:35:22 +0530

    Create action.js

---
+++ frontend/src/Redux/App/action.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import * as types from "./actionTypes";
+
+
+const { REACT_APP_API_URL } = process.env;
+const getVeg = (params) => (dispatch) => {
+    dispatch({ type: types.GET_VEG_DATA_REQUEST })
+    return axios.get("http://localhost:8000/products", params).then((r) => {
+        dispatch({ type: types.GET_VEG_DATA_SUCCESS, payload: r.data });
+    }).catch((err) => {
+        dispatch({ type: types.GET_VEG_DATA_FAILURE, payload: err });
+    })
+}
+
+
+const addProducts = (payload, headers) => (dispatch) => {
+    dispatch({ type: types.POST_DATA_REQUEST });
+    return axios.post(`${REACT_APP_API_URL}/dashboard/product/add`, payload, headers).then((res) => {
+        return dispatch({ type: types.POST_DATA_SUCCESS, payload: res.data });
+    }).catch((err) => {
+        return dispatch({ type: types.POST_DATA_FAILURE, payload: err });
+    })
+}
+
+const productData = (payload) => (dispatch) => {
+    dispatch({ type: types.GET_DATA_REQUEST });
+    return axios.get(`${REACT_APP_API_URL}/dashboard/products`, payload).then((res) => {
+        return dispatch({ type: types.GET_DATA_SUCCESS, payload: res.data });
+    }).catch((err) => {
+        return dispatch({ type: types.GET_DATA_FAILURE, payload: err });
+    })
+}
+
+
+const productDelete = (payload, headers) => (dispatch) => {
+    dispatch({ type: types.DELETE_DATA_REQUEST });
+    return axios.delete(`${REACT_APP_API_URL}/dashboard/product/delete/${payload.id}`, headers).then((res) => {
+        return dispatch({ type: types.DELETE_DATA_SUCCESS, payload: res.data });
+    }).catch((err) => {
+        return dispatch({ type: types.DELETE_DATA_FAILURE, payload: err });
+    })
+}
+
+const getProducts = () => (dispatch) => {
+    dispatch({ type: types.GET_PRODUCTS_REQUEST });
+    return axios.get(`${REACT_APP_API_URL}/data/products`).then((res) => {
+        return dispatch({ type: types.GET_PRODUCTS_SUCCESS, payload: res.data });
+    }).catch((err) => {
+        return dispatch({ type: types.GET_PRODUCTS_FAILURE, payload: err });
+    })
+}
+
+export { getVeg, addProducts, productData, productDelete, getProducts }
